c442cf55f38bb2cf2bb35646ea2251bcdfa1a9b9
Tod D. Romo <drtromo@gmail.com>
2017-05-23 13:06:21 -0400

    Made image name a variable

--- build_and_restart.sh
+++ build_and_restart.sh
@@ -2,7 +2,7 @@
 #
 # Note: Must be run via sudo
 
-
+IMAGE=$USER/jup
 
 # First, stop all running docker containers
 docker stop $(sudo docker ps -a -q)
@@ -11,7 +11,7 @@ docker stop $(sudo docker ps -a -q)
 docker rm $(sudo docker ps -a -q)
 
 # Rebuild the image for jupyter
-docker build  --rm --force-rm -t $USER/jupyter_t32:latest .
+docker build  --rm --force-rm -t $IMAGE:latest .
 
 # Get a token and record it...
 export TOKEN=$( head -c 30 /dev/urandom | xxd -p )
@@ -20,4 +20,4 @@ echo $TOKEN >token.current
 
 # Now restart everything...
 docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN  --name=proxy jupyter/configurable-http-proxy --default-target http://127.0.0.1:9999
-docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN  -v /var/run/docker.sock:/docker.sock jupyter/tmpnb python orchestrate.py --image=$USER/jupyter_t32 --container-user=jovyan --command='jupyter notebook --no-browser --port {port} --ip=0.0.0.0 --NotebookApp.base_url=/{base_path} --NotebookApp.port_retries=0 --NotebookApp.token="" --NotebookApp.disable_check_xsrf=True'
+docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN  -v /var/run/docker.sock:/docker.sock jupyter/tmpnb python orchestrate.py --image=$IMAGE --container-user=jovyan --command='jupyter notebook --no-browser --port {port} --ip=0.0.0.0 --NotebookApp.base_url=/{base_path} --NotebookApp.port_retries=0 --NotebookApp.token="" --NotebookApp.disable_check_xsrf=True'
